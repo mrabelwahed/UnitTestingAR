348a4a7a27fb956fa52c4e88e1270dd181a90c5d
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2024-09-27 16:05:14 +0300

    add release.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.droidcourses.unittestingar"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
